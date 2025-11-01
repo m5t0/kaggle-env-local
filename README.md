16fcbb3f2c9d2ea57479b4cd275fdb3afe65bf35
moto <mo_to1015@yahoo.co.jp>
2025-11-01 15:20:20 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# kaggle-env-local
+
+UVとvenvを用いたkaggle開発環境
+
+パッケージが十分かどうかと`./util/upload_dataset.py`がきちんと動くかどうかはテストがまだできていないです。
+実際に使う機会が生じたときにテストします。
---
+++ util/upload_dataset.py
@@ -0,0 +1,19 @@
+import argparse
+import os
+
+import kagglehub
+
+
+def main():
+    parser = argparse.ArgumentParser(description="script for making kaggle dataset")
+
+    parser.add_argument("dataset_name", type=str, help="dataset name")
+    parser.add_argument("folder_path", type=str, help="folder path")
+
+    args = parser.parse_args()
+
+    kagglehub.login()
+
+    kagglehub.dataset_upload(
+        os.environ["KAGGLE_USERNAME"] + "/" + args.dataset_name, args.folder_path
+    )
